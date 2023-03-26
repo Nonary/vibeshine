102a8bdfacb8244aa673758185e7bc13155e638f
Chase Payne <chase_payne@outlook.com>
2023-03-25 19:43:07 -0500

    Initial Vite Commit

---
+++ src_assets/sunshine_frontend/jest.config.js
@@ -0,0 +1,17 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleFileExtensions: ['ts', 'js', 'vue'],
+  transform: {
+    '^.+\\.vue$': '@vue/vue3-jest',
+    '^.+\\.ts$': 'ts-jest',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
