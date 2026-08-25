d5b6c426c4db90e3dec3705db76e3455eded52ca
Chase Payne <chase_payne@outlook.com>
2026-08-24 23:39:04 -0500

    build(linux): restore native build support

--- cmake/compile_definitions/linux.cmake
+++ cmake/compile_definitions/linux.cmake
@@ -359,7 +359,6 @@ list(APPEND PLATFORM_TARGET_FILES
         "${CMAKE_SOURCE_DIR}/src/platform/linux/misc.cpp"
         "${CMAKE_SOURCE_DIR}/src/platform/linux/host_stats.cpp"
         "${CMAKE_SOURCE_DIR}/src/platform/linux/audio.cpp")
-
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
     list(APPEND PLATFORM_TARGET_FILES
             "${CMAKE_SOURCE_DIR}/src/platform/linux/private_display.h"
